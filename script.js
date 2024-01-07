ff97dd36e0687fa3e700819701df660814be562c
Kevin <kevinriverag2021@gmail.com>
2024-01-06 22:00:50 -0400

    Add loop to create grid

---
+++ script.js
@@ -0,0 +1,14 @@
+let gridSize = 16;
+
+const mainContainer = document.getElementById('main-container');
+
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement('div');
+    square.style.width = '25px';
+    square.style.height = '25px';
+    square.style.border = 'solid black';
+    square.style.boxSizing = 'border-box';
+    mainContainer.appendChild(square);
+
+}
