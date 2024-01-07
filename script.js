3a9259f9964a3081e02b32305e942047227dbee2
Kevin <kevinriverag2021@gmail.com>
2024-01-07 00:00:53 -0400

    Add change grid size functionality

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 let gridSize = 16;
-
+let startingWidth = 800;
+let squareSize = startingWidth / gridSize;
+let amountOfSquares = gridSize * gridSize;
 const mainContainer = document.getElementById('main-container');
 
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.style.width = '25px';
-    square.style.height = '25px';
-    square.style.border = 'solid black';
-    square.style.boxSizing = 'border-box';
-    square.addEventListener('mouseover', changeSquareColor);
-    mainContainer.appendChild(square);
+
+
+function createGrid () {
+    squareSize = startingWidth / gridSize;
+    amountOfSquares = gridSize * gridSize;
+
+    while (mainContainer.firstChild) {  //Delete previously created grid, if any
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+
+
+    for (let i = 0; i < amountOfSquares; i++) {
+        const square = document.createElement('div');
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
+        square.style.border = 'solid black';
+        square.style.boxSizing = 'border-box';
+        square.addEventListener('mouseover', changeSquareColor);
+        mainContainer.appendChild(square);
+
+    }
 
 }
 
-function changeSquareColor (event) {
+function changeSquareColor(event) {
     event.target.style.backgroundColor = 'black';
 }
 
+function getGridSize() {
+    gridSize = prompt("Enter a new amount of squares per side");
+    createGrid();
+}
+
+const sizeButton = document.getElementById('size-button');
+
+sizeButton.addEventListener('click', getGridSize);
+
+createGrid();
